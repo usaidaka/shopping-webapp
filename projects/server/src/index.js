884343d7fdeff6bfc4fd3413a8be5fdcbd7c064e
usaid AKA <ujedkemal@gmail.com>
2023-06-28 14:07:17 +0700

    clearing cors config

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -5,14 +5,7 @@ const { join } = require("path");
 
 const PORT = process.env.PORT || 8000;
 const app = express();
-app.use(
-  cors({
-    origin: [
-      process.env.WHITELISTED_DOMAIN &&
-        process.env.WHITELISTED_DOMAIN.split(","),
-    ],
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
